7c41f84016b30b8a1f90aa9767378c2d058114ea
5KRO <tanguy.balny@orange.fr>
2023-06-01 11:14:12 +0200

    test ajout chemin

--- fitnessapp/src/pageInscription/script.js
+++ fitnessapp/src/pageInscription/script.js
@@ -4,10 +4,6 @@ document.getElementById("inscription-form").addEventListener("submit", function(
     // Récupérer les valeurs des champs
     var pseudo = document.getElementById("pseudo").value;
     var email = document.getElementById("email").value;
-    var sexe = document.getElementById("sexe").value;
-    var age = document.getElementById("age").value;
-    var poids = document.getElementById("poids").value;
-    var taille = document.getElementById("taille").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirm-password").value;
   
@@ -16,10 +12,6 @@ document.getElementById("inscription-form").addEventListener("submit", function(
     // Afficher les valeurs dans la console à des fins de démonstration
     console.log("Pseudo:", pseudo);
     console.log("Adresse email:", email);
-    console.log("Sexe:", sexe);
-    console.log("Âge:", age);
-    console.log("Poids:", poids);
-    console.log("Taille:", taille);
     console.log("Mot de passe:", password);
     console.log("Confirmation du mot de passe:", confirmPassword);
   
