b99fc255d64f9a533a5b162662befa33edd15e8f
maxdu633 <maximejay63@hotmail.com>
2023-06-07 16:01:04 +0200

    Maj contact serveur express

--- fitnessapp/src/App.js
+++ fitnessapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'; // Fichier CSS pour les styles spécifiques
+import './App.css';
 
 function FitnessJournal() {
   const [activities, setActivities] = useState([]);
@@ -7,34 +7,81 @@ function FitnessJournal() {
   const [goals, setGoals] = useState([]);
   const [statistics, setStatistics] = useState([]);
 
-  // Fetch activities from the server
   useEffect(() => {
     fetchActivities();
+    fetchStatistics();
+    fetchGoals();
   }, []);
 
-  const fetchActivities = () => {
-    // Appel à l'API pour récupérer les activités de l'utilisateur
-    // Met à jour le state 'activities' avec les données reçues
+  const fetchActivities = (userId) => {
+    fetch(`http://localhost:3000/activites/${userId}`)
+      .then((response) => response.json())
+      .then((data) => setActivities(data))
+      .catch((error) => console.error('Erreur lors de la récupération des activités :', error));
   };
 
-  const addActivity = (activity) => {
-    // Appel à l'API pour enregistrer la nouvelle activité
-    // Met à jour le state 'activities' avec la nouvelle activité ajoutée
+  const addActivity = (activity, userId) => {
+    fetch(`http://localhost:3000/activites/${userId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(activity),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Mettre à jour le state avec la nouvelle activité ajoutée
+        setActivities([...activities, data]);
+      })
+      .catch((error) => console.error('Erreur lors de l\'ajout de l\'activité :', error));
   };
 
-  const updateProgress = (value) => {
-    // Appel à l'API pour mettre à jour la progression de l'utilisateur
-    // Met à jour le state 'progress' avec la nouvelle valeur
+  const updateProgress = (value, userId) => {
+    fetch(`http://localhost:3000/objectifs/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ progress: value })
+    })
+      .then((response) => response.json())
+      .then((data) => setProgress(data.progress))
+      .catch((error) => console.error('Erreur lors de la mise à jour de la progression:', error));
   };
 
-  const setGoal = (goal) => {
-    // Appel à l'API pour définir un nouvel objectif pour l'utilisateur
-    // Met à jour le state 'goals' avec le nouvel objectif ajouté
+  const setGoal = (event, userId) => {
+    event.preventDefault();
+    const newGoal = {
+      name: event.target.elements.goalName.value
+    };
+
+    fetch(`http://localhost:3000/objectifs/${userId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newGoal)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setGoals([...goals, data]);
+        event.target.reset();
+      })
+      .catch((error) => console.error('Erreur lors de la définition de l\'objectif:', error));
   };
 
-  const fetchStatistics = () => {
-    // Appel à l'API pour récupérer les statistiques de l'utilisateur
-    // Met à jour le state 'statistics' avec les données reçues
+  const fetchGoals = (userId) => {
+    fetch(`http://localhost:3000/objectifs/${userId}`)
+      .then((response) => response.json())
+      .then((data) => setGoals(data))
+      .catch((error) => console.error('Erreur lors de la récupération des objectifs :', error));
+  };
+
+  const fetchStatistics = (userId) => {
+    fetch(`http://localhost:3000/utilisateurs/${userId}`)
+      .then((response) => response.json())
+      .then((data) => setStatistics(data.statistics))
+      .catch((error) => console.error('Erreur lors de la récupération des statistiques:', error));
   };
 
   return (
@@ -42,10 +89,10 @@ function FitnessJournal() {
       <h1>Journal de fitness</h1>
       
       <div className="activities-section">
-        <h2>Activités enregistrées</h2>
+        <h2>Activités</h2>
         <ul>
           {activities.map((activity) => (
-            <li key={activity.id}>{activity.name}</li>
+            <li key={activity._id}>{activity.name}</li>
           ))}
         </ul>
       </div>
@@ -53,41 +100,39 @@ function FitnessJournal() {
       <div className="add-activity-section">
         <h2>Ajouter une activité</h2>
         <form onSubmit={addActivity}>
-          <input type="text" placeholder="Nom de l'activité" />
+          <input type="text" name="activityName" placeholder="Nom de l'activité" />
           <button type="submit">Ajouter</button>
         </form>
       </div>
       
       <div className="progress-section">
         <h2>Progression</h2>
-        <p>Progression actuelle : {progress}%</p>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value={progress}
-          onChange={(e) => updateProgress(e.target.value)}
-        />
+        <p>Votre progression : {progress}%</p>
+        <button onClick={() => updateProgress(progress + 10)}>+10%</button>
+      </div>
+      
+      <div className="goals-section">
+        <h2>Objectifs</h2>
+        <ul>
+          {goals.map((goal) => (
+            <li key={goal._id}>{goal.name}</li>
+          ))}
+        </ul>
       </div>
       
       <div className="set-goal-section">
         <h2>Définir un nouvel objectif</h2>
         <form onSubmit={setGoal}>
-          <input type="text" placeholder="Objectif" />
+          <input type="text" name="goalName" placeholder="Nom de l'objectif" />
           <button type="submit">Définir</button>
         </form>
       </div>
       
       <div className="statistics-section">
         <h2>Statistiques</h2>
-        <ul>
-          {statistics.map((stat) => (
-            <li key={stat.id}>{stat.name}: {stat.value}</li>
-          ))}
-        </ul>
+        <p>Nombre d'activités enregistrées : {statistics.numActivities}</p>
+        <p>Nombre d'objectifs définis : {statistics.numGoals}</p>
       </div>
-      
-      <button onClick={fetchStatistics}>Actualiser les statistiques</button>
     </div>
   );
 }
