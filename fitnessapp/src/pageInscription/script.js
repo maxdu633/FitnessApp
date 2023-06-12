579a9aca1d2e586f0a9d490a5b01a32f86ffd1f3
5KRO <tanguy.balny@orange.fr>
2023-06-12 15:42:15 +0200

    test

--- fitnessapp/src/pageInscription/script.js
+++ fitnessapp/src/pageInscription/script.js
@@ -1,19 +1,45 @@
 document.getElementById("inscription-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    // Récupérer les valeurs des champs
-    var pseudo = document.getElementById("pseudo").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirm-password").value;
+  event.preventDefault();
 
-    if (password !== confirmPassword) {
+  // Récupérer les valeurs des champs
+  var pseudo = document.getElementById("pseudo").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirm-password").value;
+
+  if (password !== confirmPassword) {
       // Les mots de passe ne correspondent pas
       alert("Le mot de passe de confirmation ne correspond pas au mot de passe saisi. Veuillez réessayer.");
       return; // Arrêter l'exécution de la fonction
   }
-  
-    // Réinitialiser les champs du formulaire
-    document.getElementById("inscription-form").reset();
+
+  // Créer un objet contenant les données du formulaire
+  var formData = {
+      username: pseudo,
+      email: email,
+      password: password
+  };
+
+  // Envoyer les données à server.js en utilisant fetch()
+  fetch('http://localhost:3000/utilisateurs', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(data => {
+      // Traiter la réponse du serveur
+      console.log(data);
+      alert("Inscription réussie !");
+  })
+  .catch(error => {
+      // Gérer les erreurs
+      console.error('Erreur :', error);
+      alert("Une erreur est survenue lors de l'inscription. Veuillez réessayer.");
   });
 
+  // Réinitialiser les champs du formulaire
+  document.getElementById("inscription-form").reset();
+});
